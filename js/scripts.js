46514a5682af7bbf3beadad5b67778c3ec159691
Sean Downs <spdowns@protonmail.com>
2020-04-10 11:12:27 -0700

    is trying a display construction

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,25 @@
 //back end pizza
-function Pizza() {
-  this.price = 10;
+function Order() {
   this.size = []
 }
+console.log(Pizza)
 
-Pizza.prototype.size = function () {
-  if this.size === large += 2 
+// Pizza.prototype.size = function () {
+//   if size === large += 2 
+// }
+function Pizza(price) {
+  this.price = 10;
 }
 
-
+function displayPizzaOrder
 //front end
 
 $(document).ready(function(){
-  var finalPizza = new Pizza();
+  var finalPizza = new Pizza(pizzaSize);
   $('#form-group').submit(function(event){
   event.preventDefault();
-  var pizzaSize = $('input #size').val();
-  console.log(finalPizza)
+  var pizzaSize = $('input#size').val();
+  
+  console.log(pizzaSize)
   })
 });
