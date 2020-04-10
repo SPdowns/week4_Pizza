1d9112c4321024ace09cdf7c22ba823842354dba
Sean Downs <spdowns@protonmail.com>
2020-04-10 12:22:41 -0700

    passes thru cheese, but size is coming back undefined

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,19 @@
 //back end pizza
-function Order() {
-  this.price = 10
+function Order(price) {
+  this.price = price
 }
 
-function Pizza(size) {
+function Pizza(size, cheese) {
   this.size = size;
+  this.size = cheese;
 }
 
+Pizza.prototype.calcPrice = function (){
+  var price = 10;
+  return price;
+}
 function displayPizzaOrder(pizza) {
-var displayMyPizza = "<p>Hey, Heres a " + pizza.size + "</p>";
+var displayMyPizza = "<p>Hey, Heres a " + pizza.size + pizza.cheese + "</p>";
 $("#output-final-pizza").html(displayMyPizza);
 }
 //front end
@@ -17,9 +22,10 @@ $(document).ready(function(){
   $('#pizza-form-id').submit(function(event){
   event.preventDefault();
   var size = $("input[name=size]:checked").val();
+  var cheese = $("input[name=cheese]:checked").val();
 
-  var stepLastPizza = 
-  displayPizzaOrder(size)
+  var calcPizza = new Pizza(size, cheese)
+  displayPizzaOrder(calcPizza)
   })
 
 });
