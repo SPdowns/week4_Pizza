814eaddb6ef11911096073c8fb936b759d54b179
Sean Downs <spdowns@protonmail.com>
2020-04-10 10:02:23 -0700

    tries and makes the radio button a single selection

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 //back end pizza
 function Pizza() {
-  this.size = [];
+  this.price = 10;
+  this.size = {small, medium, large};
 
+// Pizza.prototype.sizeprice = function () {
+  
+// }
+//front end
 
-  //front end
-  $(document).ready(function(){
-    event.preventDefault();
-    $('#form-group').submit(function(event){
-    var size = $("#size").val();
+$(document).ready(function(){
+  var finalPizza = new Pizza();
+  $('#form-group').submit(function(event){
+  event.preventDefault();
+  var pizzaSize = $('#size').val();
+  console.log(pizzaSize)
+  })
+});
